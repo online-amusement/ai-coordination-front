f27edd84e34af6768d9124a88f5a43bdf2ef9e30
olp <t.miyai0001@gmail.com>
2023-04-17 09:54:24 +0900

    headerの作成とindexページの作成

---
+++ components/CommonHeader.vue
@@ -0,0 +1,93 @@
+<template>
+    <header class="header">
+            <div class="logo-area" @click="backHome()">
+                <img class="logo" src="/images/logo.png">
+            </div>
+            <div v-if="$auth.loggedIn" class="menu-area" @click="openMenu">
+                <img class="menu-btn" src="/images/menu.png">
+            </div>
+            <div v-else class="login-btn">
+                <nuxt-link to="/login">ログイン</nuxt-link>
+            </div>
+            <div class="menu" v-bind:class="{'is-active' : open }">
+                <div class="menu__item">TOP</div>
+                <div class="menu__item">ABOUT</div>
+                <div class="menu__item">BLOG</div>
+                <div class="menu__item">CONTACT</div>
+            </div>
+    </header>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+@Component({
+})
+export default class CommonHeader extends Vue {
+open: boolean = false;
+
+backHome() {
+    this.$router.push('/home');
+}
+openMenu() {
+    this.open = !this.open;
+    console.log(this.open)
+}
+}
+</script>
+<style lang="scss">
+.logo {
+    width: 80px;
+    height: auto;
+    margin-left: 10px;
+}
+.menu-btn {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 3;
+    width: 60px;
+    height: auto;
+    margin-right: 10px;
+}
+
+.header {
+    margin: 20px auto;
+    display: flex;
+    justify-content: space-between;
+    height: 90px;
+    align-items: center;
+    border-bottom: 1px solid #888888;
+}
+.menu{
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  width: 50vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #888888;
+}
+.menu__item{
+  width: 100%;
+  height: auto;
+  padding: .5em 1em;
+  text-align: center;
+  color: #fff;
+  box-sizing: border-box;
+}
+.menu{
+  transform: translateX(50vw);
+  transition: all .3s linear;
+}
+/* アニメーション後のメニューの状態 */
+.menu.is-active{
+  transform: translateX(0);
+}
+.login-btn {
+    margin-right: 30px;
+    cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,129 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <CommonHeader />
+    <div class="area-title">
+      <h2>Aiコーディネート使い方</h2>
+    </div>
+    <div class="main-container">
+      <div class="step-1">
+        <div class="step-1-title">
+          <h3 class="_step">ステップ1.</h3>
+        </div>
+        <div class="image-index">
+          <img class="description-1" src="/images/description-1.png">
+          <div class="detail">
+            <p>まずは自分の等身大の画像をアップデートしよう。</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="step-2">
+        <div class="step-1-title">
+          <h3 class="_step">ステップ2.</h3>
+        </div>
+        <div class="image-index">
+          <img class="description-1" src="/images/description-2.png">
+          <div class="detail">
+            <p>試着したい洋服を選択しよう。</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="step-3">
+        <div class="step-1-title">
+          <h3 class="_step">ステップ3.</h3>
+        </div>
+        <div class="image-index">
+          <img class="description-1" src="/images/description-2.png">
+          <div class="detail">
+            <p>試着したい洋服を選択しよう。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="account-area">
+      <nuxt-link to="/login" class="login-btn">
+        <h3 class="login-page">さっそく使ってみる</h3>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Vue } from 'nuxt-property-decorator';
+import CommonHeader from '~/components/CommonHeader.vue';
 
-export default Vue.extend({
-  name: 'IndexPage'
+@Component({
+  components: {
+    CommonHeader,
+  }
 })
+  
+export default class INDEX extends Vue {
+
+}
 </script>
+<style lang="scss">
+.main-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  border: 1px solid #333;
+}
+.area-title {
+  text-align: center;
+  color: #333333;
+}
+.step-1-title {
+  max-width: 150px;
+  text-align: center;
+  color: #333333;
+  border-bottom:1px solid #333333;
+  margin-top: 50px;
+}
+.description-1 {
+  width: 200px;
+}
+.image-index {
+  margin: 15px 100px;
+  display: flex;
+}
+.step-1 {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.step-2 {
+  max-width: 800px;
+  margin: 0 auto;
+  margin-top: 100px;
+}
+.step-3 {
+  max-width: 800px;
+  margin: 0 auto;
+  margin-top: 100px;
+  margin-bottom: 50px;
+}
+.account-area {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  .login-page {
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+    border-bottom: 1px solid #333;
+    max-width: 200px;
+    margin-bottom: 50px;
+  }
+}
+.login-btn {
+  cursor: pointer;
+  text-decoration: none;
+}
+@media screen and (max-width:480px){
+  .image-index {
+    margin: 15px 100px;
+    display: block;
+  } 
+}
+</style>
